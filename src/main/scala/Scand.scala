ac00ef013587acf7d1c225b45784f13fc49e5e8b
Moses Nakamura <nnythm@gmail.com>
2012-08-20 09:56:04 -0400

    all unit tests failing, fuck

--- src/main/scala/Scand.scala
+++ src/main/scala/Scand.scala
@@ -3,51 +3,66 @@ import scala.util.parsing.combinator.RegexParsers
 
 object Scand extends RegexParsers {
   def andWithRepetition[T](list: List[Parser[T]],
-      reducer: (T, T) => T,
-      default: T): Parser[T] = andWithRepetition(List(), list, reducer, default)
+    reducer: (T, T) => T,
+    default: T): Parser[T] = andWithRepetition(ParserHelper(List(), list, reducer, default))
 
   def andWithoutRepetition[T](list: List[Parser[T]],
-      reducer: (T, T) => T,
-      default: T): Parser[T] = parseList(list, reducer, default) getOrElse success(default)
+    reducer: (T, T) => T,
+    default: T): Parser[T] =
+      andWithoutRepetition(ParserHelper(List(), list, reducer, default))
 
-  def andWithRepetition[T](parsed: List[Parser[T]],
-      unparsed: List[Parser[T]],
-      reducer: (T, T) => T,
-      default: T): Parser[T] = {
-        val unparsedProgress = parseUnparsed(parsed, unparsed, reducer, default)
-        val parsedProgress = parseParsed(parsed, unparsed, reducer, default) getOrElse
-          success(default)
-        unparsedProgress map (_ | parsedProgress) getOrElse parsedProgress
+  def andWithoutRepetition[T](helper: ParserHelper[T]): Parser[T] =
+      parseList(helper) getOrElse success(helper.default)
+
+  private[this] def andWithRepetition[T](helper: ParserHelper[T]): Parser[T] = {
+    val unparsedProgress = parseUnparsed(helper)
+    val parsedProgress = parseParsed(helper) getOrElse success(helper.default)
+    unparsedProgress map (_ | parsedProgress) getOrElse parsedProgress
+  }
+
+  private[this] def parseParsed[T](helper: ParserHelper[T]) =
+    (helper.parsed reduceOption (_ | _)) map (
+      _ ~ andWithRepetition(helper) map {
+        applyReducer(_, helper.reducer)
       }
+    )
 
-  def parseParsed[T](parsed: List[Parser[T]],
-      unparsed: List[Parser[T]],
-      reducer: (T, T) => T,
-      default: T) = (parsed reduceOption (_ | _)) map (
-        _ ~ andWithRepetition(parsed, unparsed, reducer, default) map {
-          case f ~ s => reducer(f, s)
-        }
-      )
+  private[this] def parseUnparsed[T](helper: ParserHelper[T]) = (helper.unparsed map {parser =>
+    parser ~ andWithRepetition(helper.useParser(parser)) map (
+      applyReducer(_, helper.reducer)
+    )
+  }) reduceOption (_ | _)
 
-  def parseUnparsed[T](parsed: List[Parser[T]],
-      unparsed: List[Parser[T]],
-      reducer: (T, T) => T,
-      default: T) = (unparsed map {parser =>
-        parser ~ andWithRepetition(parser :: parsed, unparsed filterNot (_ == parser), reducer, default) map {
-          case f ~ s => reducer(f, s)
-        }
-      }) reduceOption (_ | _)
+  private[this] def parseList[T](helper: ParserHelper[T]) = (helper.unparsed map
+      (andThenParse(_, helper))
+    ) reduceOption (_ | _)
 
-  def parseList[T](unparsed: List[Parser[T]],
-      reducer: (T, T) => T,
-      default: T) = (unparsed map {parser =>
-        parser ~ andWithoutRepetition(unparsed filterNot (_ == parser), reducer, default) map {
-          case f ~ s => reducer(f, s)
-        }
-      }) reduceOption (_ | _)
-
-  def reduceOrWithDefault[U](list: List[Parser[U]], default: Parser[U]): Parser[U] =
+  private[this] def reduceOrWithDefault[U](list: List[Parser[U]], default: Parser[U]): Parser[U] =
     list reduceOption (_ | _) getOrElse default
 
   def stringParser(string: String): Parser[String] = string
+
+  private[this] def andThenParse[T](parser: Parser[T],
+    helper: ParserHelper[T]): Parser[T] = {
+    parser ~ andWithoutRepetition(helper.useParser(parser)) map (
+      applyReducer(_, helper.reducer)
+    )
+  }
+
+  private[this] def applyReducer[T](concat: ~[T, T], reducer: (T, T) => T) = concat match {
+    case f ~ s => reducer(f, s)
+  }
+
+  case class ParserHelper[T](parsed: List[Parser[T]],
+    unparsed: List[Parser[T]],
+    reducer: (T, T) => T,
+    default: T) {
+    def useParser(parser: Parser[T]) = this.copy(parsed = parser :: this.parsed,
+      unparsed = this.unparsed filterNot (_ == parser))
+  }
+
+  object ParserHelper {
+    def apply[T](list: List[Parser[T]], reducer: (T, T) => T, default: T): ParserHelper[T] =
+      ParserHelper(List(), list, reducer, default)
+  }
 }
--- src/test/scala/ScandSpec.scala
+++ src/test/scala/ScandSpec.scala
@@ -3,9 +3,10 @@ import org.scalatest.FunSpec
 import org.scalatest.junit.JUnitRunner
 import scala.util.parsing.combinator.RegexParsers
 import scala.util.parsing.input.CharSequenceReader
+import org.scalatest.matchers.ShouldMatchers
 
 @RunWith(classOf[JUnitRunner])
-class ScandSpec extends FunSpec {
+class ScandSpec extends FunSpec with ShouldMatchers {
   describe("Scand") {
     lazy val fooParser = Scand.stringParser("foo")
     lazy val barParser = Scand.stringParser("bar")
@@ -22,64 +23,64 @@ class ScandSpec extends FunSpec {
     describe("andWithRepetition") {
       it("should parser a single parser normally") {
         val parser = Scand.andWithRepetition(List(fooParser), concat, "")
-        parser(foo).get === "foo"
+        assert(parser(foo).get === "foo")
       }
       it("should parser two parsers forwards") {
         val parser = Scand.andWithRepetition(List(fooParser, barParser), concat, "")
-        parser(foobar).get === "foobar"
+        assert(parser(foobar).get === "foobar")
       }
       it("should parser two parsers backwards") {
         val parser = Scand.andWithRepetition(List(fooParser, barParser), concat, "")
-        parser(barfoo).get === "barfoo"
+        assert(parser(barfoo).get === "barfoo")
       }
       it("should parser three parsers forwards") {
         val parser = Scand.andWithRepetition(List(fooParser, barParser, bazParser), concat, "")
-        parser(foobarbaz).get === "foobarbaz"
+        assert(parser(foobarbaz).get === "foobarbaz")
       }
       it("should parser three parsers in every configuration") {
         for (list <- Util.reorder(List(fooParser, barParser, bazParser))) {
           val parser = Scand.andWithRepetition(list, concat, "")
-          parser(foobarbaz).get === "foobarbaz"
+          assert(parser(foobarbaz).get === "foobarbaz")
         }
       }
       it("should handle repetition with only one arg.") {
         val parser = Scand.andWithRepetition(List(fooParser), concat, "")
-        parser(foofoo).get === "foofoo"
+        assert(parser(foofoo).get === "foofoo")
       }
       it("should handle repetition with two args, in order.") {
         val parser = Scand.andWithRepetition(List(fooParser, barParser), concat, "")
-        parser(foofoobar).get === "foofoobar"
+        assert(parser(foofoobar).get === "foofoobar")
       }
       it("should handle repetition with two args, out of order.") {
         val parser = Scand.andWithRepetition(List(fooParser, barParser), concat, "")
-        parser(barfoobar).get === "barfoobar"
+        assert(parser(barfoobar).get === "barfoobar")
       }
       it("should handle repetition with three args, out of order.") {
         val parser = Scand.andWithRepetition(List(fooParser, barParser, bazParser), concat, "")
-        parser(barbazfoobazbar).get === "barbazfoobazbar"
+        assert(parser(barbazfoobazbar).get === "barbazfoobazbar")
       }
     }
     describe("andWithoutRepetition") {
       it("should parser a single parser normally") {
         val parser = Scand.andWithoutRepetition(List(fooParser), concat, "")
-        parser(foo).get === "foo"
+        assert(parser(foo).get === "foo")
       }
       it("should parser two parsers forwards") {
         val parser = Scand.andWithoutRepetition(List(fooParser, barParser), concat, "")
-        parser(foobar).get === "foobar"
+        assert(parser(foobar).get === "foobar")
       }
       it("should parser two parsers backwards") {
         val parser = Scand.andWithoutRepetition(List(fooParser, barParser), concat, "")
-        parser(barfoo).get === "barfoo"
+        assert(parser(barfoo).get === "barfoo")
       }
       it("should parser three parsers forwards") {
         val parser = Scand.andWithoutRepetition(List(fooParser, barParser, bazParser), concat, "")
-        parser(foobarbaz).get === "foobarbaz"
+        assert(parser(foobarbaz).get === "foobarbaz")
       }
       it("should parser three parsers in every configuration") {
         for (list <- Util.reorder(List(fooParser, barParser, bazParser))) {
           val parser = Scand.andWithoutRepetition(list, concat, "")
-          parser(foobarbaz).get === "foobarbaz"
+          assert(parser(foobarbaz).get === ("foobarbaz"))
         }
       }
     }
@@ -90,14 +91,14 @@ class ScandSpec extends FunSpec {
       val twoList = List(0, 1)
       val threeList = List(0, 1, 2)
       it("Should reorder a 1-list") {
-        Util.reorder(oneList) === List(oneList)
+        assert(Util.reorder(oneList).toSet === Set(oneList))
       }
       it("Should reorder a 2-list") {
-        Util.reorder(twoList) === List(oneList, oneList.reverse)
+        assert(Util.reorder(twoList).toSet === Set(twoList, twoList.reverse))
       }
       it("Should reorder a 3-list") {
-        Util.reorder(threeList) === List(threeList, threeList.reverse, List(1, 0, 2),
-            List(2, 0, 1), List(1, 2, 0), List(0, 2, 1))
+        assert(Util.reorder(threeList).toSet === Set(threeList, threeList.reverse, List(1, 0, 2),
+            List(2, 0, 1), List(1, 2, 0), List(0, 2, 1)))
       }
     }
   }
@@ -105,11 +106,9 @@ class ScandSpec extends FunSpec {
 
 object Util {
   def reorder[T](list: List[T]): List[List[T]] = list match {
-    case Nil => List()
-    case nonempty => nonempty map {elt =>
-      reorder(list filterNot (_ == elt)) flatMap {
-        elt :: _
-      }
+    case Nil => List(List())
+    case nonempty => nonempty flatMap {elt =>
+      reorder(list filterNot (_ == elt)) map (elt :: _)
     }
   }
 }
